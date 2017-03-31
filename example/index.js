b8b44c3b97bd23fe1070e3adc9fcafbc18e7e218
Max Nachlinger <max.nachlinger@gmail.com>
2017-03-30 23:27:22 -0700

    Updated README, added an example

--- example/index.js
+++ example/index.js
@@ -1,28 +1,5 @@
-# good-json-schema-filter
-A Good log object transform stream which filters via json-schema
+'use strict'
 
-[![standard][standard-image]][standard-url]
-[![travis][travis-image]][travis-url]
-[![npm][npm-image]][npm-url]
-
-[travis-image]: https://travis-ci.org/maxnachlinger/good-json-schema-filter.svg?branch=master
-[travis-url]: https://travis-ci.org/maxnachlinger/good-json-schema-filter
-[npm-image]: https://img.shields.io/npm/v/good-json-schema-filter.svg?style=flat
-[npm-url]: https://npmjs.org/package/good-json-schema-filter
-[standard-image]: https://img.shields.io/badge/code%20style-standard-brightgreen.svg
-[standard-url]: http://standardjs.com/
-
-### Installation:
-```
-npm i good-json-schema-filter --save
-```
-
-### What problem does this solve?
-This library allows you to omit keys or entire log objects matched via json-schema syntax.
-
-### Example
-Omitting keys
-```javascript
 const Stream = require('stream')
 const SafeJson = require('good-squeeze').SafeJson
 const GoodJsonSchemaFilter = require('../')
@@ -86,9 +63,3 @@ mockLogStream.push({
   foo: 'bar',
   baz: 'bar'
 })
-```
-This prints:
-```javascript
-{"type":"filtered-type"}
-{"type":"this-will-be-passed-through","foo":"bar","baz":"bar"}
-```
